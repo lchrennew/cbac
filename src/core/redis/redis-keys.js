b581c4853b1a618b60b965d3c8585d300e1b92df
lichun <lchrennew@126.com>
2022-02-28 10:34:52 +0800

    extract redis implement

--- src/core/redis/redis-keys.js
+++
@@ -1,9 +0,0 @@
-import * as crypto from "crypto";
-
-export const globalValidators = () => `{global}:validators`
-export const validatorClaims = () => `{validator}:claims`
-export const validatorAliases = () => `{validator}:aliases`
-export const claimSha256 = content => crypto.createHash('sha256').update(content).digest('hex')
-export const accessProps = access => `{access}:props:${access}`
-export const accessValidators = access => `{access}:validators:${access}`
-export const accesses =()=>`{access}:info`
--- src/core/redis/redis-keys.test.js
+++
@@ -1,8 +0,0 @@
-import { claimSha256 } from "./redis-keys.js";
-
-describe('redis-keys.js', () => {
-    test('generate claim', () => {
-        expect(claimSha256('123')).toEqual('a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3')
-        expect(claimSha256('')).toEqual('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
-    })
-})
